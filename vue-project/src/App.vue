cef598b6d25cd1070e71da5cb9b0bcdcaba8c6a0
Joe Gene Quesada <52223592+apcjlquesada@users.noreply.github.com>
2025-01-23 15:07:36 +0800

    updated

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,9 +2,10 @@
 
 <template>
    <h1>Food</h1>
-  <food-item/>
-  <food-item2/>
-  <food-item/>
+  <food-item/> <br>
+  <food-item2/> <br>
+  <food-item/>  <br>
+  <food-item2/> <br>
 </template>
 
 <style></style>
