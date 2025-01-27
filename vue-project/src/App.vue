40288bfe0fb9ea2b9610fb2c17bdb3e85d150e06
Joe Gene Quesada <52223592+apcjlquesada@users.noreply.github.com>
2025-01-27 14:14:15 +0800

    removed <br>

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -3,10 +3,10 @@
 <template>
    <h1>Food</h1>
   <personal-profile/> <br>
-  <food-item/> <br>
-  <food-item2/> <br>
-  <food-item/>  <br>
-  <food-item2/> <br>
+  <food-item/> 
+  <food-item2/>
+  <food-item/> 
+  <food-item2/> 
 </template>
 
 <style></style>
