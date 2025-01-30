d433baba4c33be4ef6fb2577eec315fcfeb0d868
Joe Gene Quesada <52223592+apcjlquesada@users.noreply.github.com>
2025-01-30 06:47:33 +0000

    Query data from the app

---
+++ vue-project/src/lib/supabaseClient.js
@@ -0,0 +1,3 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient('https://ljatdwddotduzmamsxzv.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxqYXRkd2Rkb3RkdXptYW1zeHp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgwMjY4OTksImV4cCI6MjA1MzYwMjg5OX0.BiB_o73NpIkh99DEgMUT2N2kIZDnxy1VaWP0KahW-_Q')
